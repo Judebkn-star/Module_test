580fa6eb39a1cd94f528a8bf9632787a80289c21
Jude <jbuyikana@eugeniaschool.com>
2025-10-02 13:13:04 +0000

    1st_commit

---
+++ main.py
@@ -0,0 +1,79 @@
+
+
+# age = 22, 
+# print(f"Age: {age}, Type: {type(age)}")
+# height= 1.90, 
+# print(f"Taille: {height}, Type: {type(height)}")
+# name = "Jude Buyikana" 
+# print(f"Nom: {name}, Type: {type(name)}")
+# is_student = True
+# print(f"Student: {is_student}, Type: {type(is_student)}") 
+
+#Partie 2
+
+# a = 4
+# b = 2
+# print(a + b)
+# print(a - b)
+# print(a*b)
+# print(a/b)
+# print(a%b)
+# print(a**b)
+
+
+# # Partie 3 
+
+# first_name = "Jude"
+# last_name= "Buyikana"
+
+# full_name = first_name + last_name 
+# print(full_name) 
+
+# # full_name = f"{first_name} {last_name}"
+# print(full_name.upper())
+
+# print(len(full_name))
+
+# lst = ("Jude", "Buyikana", "est", "le", "roi") 
+# print(lst)
+
+
+# # PARTIE 4
+# a_string = "42"
+# print(a_string)
+# print()
+
+# a_integer = int(a_string)
+# print(type(a_integer))
+
+# a_float = 4.2
+# print(a_float)
+# print(type(a_float))
+
+# a_integer = int(a_float)
+# print(type(a_integer))
+
+# b_integer = 543
+# b_string = str(b_integer)
+# print(type(b_string))
+
+
+#Partie 5
+
+vrai = True
+false = False
+
+print(type(vrai))
+
+and_result = vrai and false
+or_result = vrai or false
+not_result = not vrai
+
+print("AND:", and_result)   
+print("OR:", or_result)     
+print("NOT:", not_result) 
+
+
+#git add <nom du fichier>
+#git commit -m " module 1 finish"
+# git push 
